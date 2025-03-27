98169e19f95b0fe4a082d664c3e4901a6e8024ab
unknown <kaabaoui.othman@gmail.com>
2025-03-27 04:18:06 +0100

    Modif auth.service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,6 +5,11 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { logger } from "../utils/logger.js";
+enum Role {
+  ETUDIANT = "ETUDIANT",
+  ENCADRANT = "ENCADRANT",
+  // Add other roles if necessary
+}
 
 dotenv.config();
 const jwtSecret = process.env.JWT_SECRET as string;
@@ -14,12 +19,17 @@ export const registerUser = async (
   username: string,
   email: string,
   password: string,
-  role: string
+  role: Role = Role.ETUDIANT // Valeur par défaut
 ) => {
   const hashedPassword = await bcrypt.hash(password, saltRounds);
   try {
     const user = await prisma.user.create({
-      data: { username, email, password: hashedPassword, role },
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+        role,
+      },
     });
     return user;
   } catch (error) {
