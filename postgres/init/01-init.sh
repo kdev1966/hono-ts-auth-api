edde3fdbc1c1deba046289560838a52bb6f2ba43
unknown <kaabaoui.othman@gmail.com>
2025-04-02 21:09:34 +0100

    Modif files

--- postgres/init/01-init.sh
+++ postgres/init/01-init.sh
@@ -2,11 +2,17 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER db_root_user WITH SUPERUSER PASSWORD 'T4m\$8gLq#vR3\!pK9';
+    -- No backslashes needed for $ or ! in single-quoted passwords
+    CREATE USER db_root_user WITH SUPERUSER PASSWORD 'T4m$8gLq#vR3!pK9';
     CREATE DATABASE encadrinyv01;
     GRANT ALL PRIVILEGES ON DATABASE encadrinyv01 TO db_root_user;
     
-    CREATE USER encadrin_api_usr WITH PASSWORD 'T4m\$8gLqXvR3\!pK9';
-    GRANT CONNECT ON DATABASE encadrinyv01 TO encadrin_api_usr;
-    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO encadrin_api_usr;
+    -- Password matches .env's URL-encoded credentials (T4m$8gLqXvR3!pK9)
+    CREATE USER encadrin_api_usr WITH PASSWORD 'T4m$8gLqXvR3!pK9';
+    
+    -- Grant necessary privileges for Prisma
+    GRANT CONNECT, CREATE ON DATABASE encadrinyv01 TO encadrin_api_usr;
+    GRANT USAGE ON SCHEMA public TO encadrin_api_usr;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO encadrin_api_usr;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO encadrin_api_usr;
 EOSQL
---
+++ postgres/init/01-init.sql
@@ -0,0 +1,3 @@
+CREATE USER encadrin_api_usr WITH PASSWORD 'T4m$8gLqXvR3!pK9';
+GRANT ALL PRIVILEGES ON DATABASE encadrinyv01 TO encadrin_api_usr;
+ALTER DATABASE encadrinyv01 OWNER TO encadrin_api_usr;
