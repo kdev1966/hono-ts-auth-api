650dbb3d802727b2f1827ab4d72ef740be776e1f
unknown <kaabaoui.othman@gmail.com>
2025-04-03 17:11:51 +0100

    Modif ++

--- postgres/init/01-init.sh
+++ postgres/init/01-init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -e
+set -eo pipefail
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER encadrin_api_usr WITH PASSWORD 'T4m$8gLqXvR3!pK9'; # Version brute
-    CREATE DATABASE encadrinyv01;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE ROLE encadrin_api_usr WITH LOGIN PASSWORD 'T4m$8gLqXvR3!pK9';
+    ALTER ROLE encadrin_api_usr CREATEDB;
     GRANT ALL PRIVILEGES ON DATABASE encadrinyv01 TO encadrin_api_usr;
 EOSQL
