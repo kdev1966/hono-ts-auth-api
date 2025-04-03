2c0019b3725707b6f367ff7b45847f0d9ae23c72
unknown <kaabaoui.othman@gmail.com>
2025-04-03 15:56:54 +0100

    Modif 03-04-2025

--- postgres/init/01-init.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER encadrin_api_usr WITH PASSWORD 'T4m$8gLqXvR3!pK9';
-GRANT ALL PRIVILEGES ON DATABASE encadrinyv01 TO encadrin_api_usr;
-ALTER DATABASE encadrinyv01 OWNER TO encadrin_api_usr;
