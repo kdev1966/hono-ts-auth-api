208a8e78f8a8bd8bfe91edf3b750332f06a8db94
unknown <kaabaoui.othman@gmail.com>
2025-04-03 17:36:40 +0100

    01-init.sh

--- postgres/init/01-init.sh
+++ postgres/init/01-init.sh
@@ -2,7 +2,8 @@
 set -eo pipefail
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE ROLE encadrin_api_usr WITH LOGIN PASSWORD 'T4m\$8gLqXvR3\!pK9';  # Échappement des caractères spéciaux
-    ALTER DATABASE encadrinyv01 OWNER TO encadrin_api_usr;
+    CREATE ROLE encadrin_api_usr WITH LOGIN PASSWORD '${API_DB_PASSWORD}';
+    CREATE DATABASE encadrinyv01;
     GRANT ALL PRIVILEGES ON DATABASE encadrinyv01 TO encadrin_api_usr;
+    ALTER DATABASE encadrinyv01 OWNER TO encadrin_api_usr;
 EOSQL
